fd34bbc658d3855150ae0f7f3b2bcccad0288722
kalema1 <kalemar8@gmail.com>
2024-01-29 13:37:22 +0300

    feat(html markup): add html markup template
    - added markup html template for our application.
    - linked the css file to our markup file for styling
    - linked the travelListController.js file to the markup file to dynamically
      configure the application.

--- travelListController.js
+++ travelListController.js
@@ -1,1 +1,1 @@
-// this is a controller file that acts as a bridge between the the state and the view of the application
+// this is a controller file that acts as a bridge between the state and the view of the application
