8828c96d0ba5e6ec37681c621ed7a1cbc385415f
kalema1 <kalemar8@gmail.com>
2024-02-03 20:57:47 +0300

    feat(renderStatisticsView): add method to render statistics markup
    - created generateStatisticsMarkup method which return the statistics
      markup.
    - created a renderStatisticsView method which renders the markup.
    - selected the statistics container to contain the statistics data.
    - created the statisticsData object which contain the statistics data.
    - removed the temporary statistics markup from the html file.
    
    This update ensures that the statistics of the list can be rendered
    dynamically.

--- listStatisticsView.js
+++ listStatisticsView.js
@@ -1,3 +1,27 @@
 // this file is responsible for the list statistics view of the application
 
-class ListStatisticsView {}
+class ListStatisticsView {
+  constructor() {
+    this.statisticsElement = document.getElementById("statistics");
+    this.statisticData;
+  }
+
+  /**
+   * renders statistics view
+   * @param {object} - data string to rendered
+   */
+  #renderStatisticsView(data) {
+    this.data = data;
+    const statisticsmarkup = this.#generateStatisticsMarkup(this.data);
+    this.statisticsElement.innerHTML = "";
+    this.statisticsElement.insertAdjacentElement("afterend", statisticsmarkup);
+  }
+
+  #generateStatisticsMarkup(data) {
+    const markup = `
+        <em>You have ${data.travelListlength} items on your list, and you already packed ${travelitemspacked} (${pecentageOftravelitemspacked}%)</em>
+    `;
+    return markup;
+  }
+}
+export const listStatisticsView = new ListStatisticsView();
